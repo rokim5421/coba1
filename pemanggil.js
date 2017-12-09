2e9d5ec942c4e01ae18f92f3931937ae7e0eed0a
rokim5421 <34340573+rokim5421@users.noreply.github.com>
2017-12-09 12:35:52 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -22,8 +22,7 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2017/12/car-buyers-get-bigger-discounts-in-2017.html"}
-if(x=="2"){xxx="/2017/12/jeep-wrangler-redesigned-fiat-chrysler.html"}
-if(x=="3"){xxx="/2017/12/teknologi-terkini-berbasis-website.html"}
+
 if(x=="4"){xxx="/2017/12/mengoptimalkan-tayangan-iklan-dengan.html"}a_to_ve[i].href="https://safelink-sab-soft.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
